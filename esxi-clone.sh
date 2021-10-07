a8c69925e3a53409c113f9f25eccff7be8edbf94
leon-ops <leon-ops@users.noreply.github.com>
2021-10-07 17:39:45 +0200

    Update esxi-clone.sh

--- esxi-clone.sh
+++ esxi-clone.sh
@@ -1,3 +1,8 @@
+#Prerequisite
+# 1. Esxi installed
+# 2. connect through ssh on the esxi
+# 3. cd to DATASTORE path
+#./clone.sh vm-src vm-dst, then go in UI, register the vm-dst, click on I copied the vm, and power on. That's all folks!
 # syntax: ./clone.sh vm-src vm-dest
 # DATA_STORE_PATH=/vmfs/volumes/myDataStore
 #cd $DATA_STORE_PATH
@@ -6,7 +11,8 @@ date
 set -x
 SRC=$1
 DST=$2
-cp -dpR ./$SRC $DST &&  cd $DST &&  vmkfstools -E $SRC.vmdk $DST.vmdk && mv $SRC.vmx $DST.vmx &&  sed -i "s/$SRC/$DST/" $DST.vmx
+cp -R ./$SRC $DST &&  cd $DST &&  vmkfstools -E $SRC.vmdk $DST.vmdk && mv $SRC.vmx $DST.vmx &&  sed -i "s/$SRC/$DST/" $DST.vmx
 
 echo "end of cloning"
 date
+
