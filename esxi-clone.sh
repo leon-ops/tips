e3da8e4ece46873aa911503f962e828dbba7f900
leon-ops <leon-ops@users.noreply.github.com>
2021-10-07 17:12:01 +0200

    Update esxi-clone.sh

--- esxi-clone.sh
+++ esxi-clone.sh
@@ -1,7 +1,7 @@
 # syntax: ./clone.sh vm-src vm-dest
 # DATA_STORE_PATH=/vmfs/volumes/myDataStore
+#cd $DATA_STORE_PATH
 set -x
-cd $DATA_STORE_PATH
 SRC=$1
 DST=$2
 cp -dpR ./$SRC $DST &&  cd $DST &&  vmkfstools -E $SRC.vmdk $DST.vmdk && mv $SRC.vmx $DST.vmx &&  sed -i "s/$SRC/$DST/" $DST.vmx
